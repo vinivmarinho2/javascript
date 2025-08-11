f7e6af12bc5e418dccea21fdd08c1e49bb2d32de
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-11 15:02:35 -0400

    Finalizei desafio do jokenpo

--- meus_exercicios/exemplo_05/jokenpo.js
+++ meus_exercicios/exemplo_05/jokenpo.js
@@ -1,6 +1,7 @@
 let pontos_usuario = 0
 let pontos_computador = 0
-function computador() {
+let empate = 0
+function computador() { // Sorteia a escolha do computador
     escolha = Math.floor(Math.random() * 3) // Sorteia entre "0, 1, 2"
     if (escolha === 0) {
         escolha = "pedra"
@@ -12,17 +13,17 @@ function computador() {
     return escolha
 }
 function usuario() {
-    escolha = prompt("pedra, papel ou tesoura?").toLowerCase()
+    escolha = prompt("pedra, papel ou tesoura?").toLowerCase() // Recebe o input do usuário
     return escolha
 }
-escolha_usuario = usuario()
-escolha_computador = computador()
+
 
 function rodada(escolha_usuario, escolha_computador) {
         if (escolha_computador === "tesoura" && escolha_usuario === "papel" || escolha_computador === "papel" && escolha_usuario === "pedra" || escolha_computador === "pedra" && escolha_usuario === "tesoura") {
             pontos_computador += 1
             alert(` Usuário: ${escolha_usuario} \n Computador: ${escolha_computador} \n Você perdeu!`)
         } else if (escolha_computador == escolha_usuario) {
+            empate += 1
             alert(`Usuário: ${escolha_usuario}\nComputador: ${escolha_computador}\nEmpatou!`)
         } else {
             pontos_usuario += 1
@@ -31,7 +32,13 @@ function rodada(escolha_usuario, escolha_computador) {
         return [pontos_computador, pontos_usuario] // retorna um array
 }
 function jogar(){
-    
+    for(let contador = 1; contador <= 5; contador++){
+        let escolha_usuario = usuario()
+        let escolha_computador = computador()
+        rodada(escolha_usuario, escolha_computador)
+    }
+
+    alert(`Vitórias: ${pontos_usuario}\nDerrotas: ${pontos_computador}\nEmpates: ${empate}`)
 }
-rodada(escolha_usuario, escolha_computador)
+jogar()
 
