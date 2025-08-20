958d2de8e23dbd045d150266af9aa4ddafdd6fc0
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-20 14:55:27 -0400

    Finalizei o exercício

--- meus_exercicios/exemplo_05/jokenpo.js
+++ meus_exercicios/exemplo_05/jokenpo.js
@@ -3,20 +3,36 @@ let pontos_computador = 0
 let empate = 0
 let botoes = document.querySelectorAll("button")
 let resultado = document.querySelector("div#resultado")
+let placar = document.querySelector ("div#placar")
 botoes.forEach((botao) => { //"ForEach" percorre todos os itens da variável "botoes"
     // Adiciona um evento de clique para cada botão 
     botao.addEventListener("click", (elemento) => { // O "elemento" em parênteses representa o objeto do evento que ocorreu
         let escolha_computador = computador()
          if (escolha_computador === "tesoura" && elemento.target.id === "papel" || escolha_computador === "papel" && elemento.target.id === "pedra" || escolha_computador === "pedra" && elemento.target.id === "tesoura") {
-            pontos_computador += 1
             resultado.innerHTML = `Computador: ${escolha_computador}, Usuário: ${elemento.target.id}, Você perdeu!`
             pontos_computador += 1
          } else if (escolha_computador === elemento.target.id) {
             resultado.innerHTML = `Empatou! Ambos escolheram ${escolha_computador}`
+            empate += 1
          } else {
             resultado.textContent = `Computador: ${escolha_computador}. Usuário: ${elemento.target.id}. Você venceu!`
             pontos_usuario += 1
+            
          }
+        placar.innerHTML = `<p>Usuário: ${pontos_usuario} <br>Computador: ${pontos_computador} <br>Empates: ${empate}`
+        // Comando de parada caso alguém chegue aos 5 pontos
+        if (pontos_usuario === 5) {
+            resultado.textContent = "Parabéns!! Você ganhou o jogo!"
+            pontos_computador = 0 // Quando o jogo acaba, os pontos são reiniciados
+            pontos_usuario = 0
+            empate = 0
+        } else if (pontos_computador === 5) {
+            resultado.textContent = "Você perdeu!"
+            pontos_computador = 0
+            pontos_usuario = 0
+            empate = 0
+
+        }
     })
 })
 
@@ -31,6 +47,8 @@ function computador() { // Sorteia a escolha do computador
     }
     return escolha
 }
+
+// Só preciso salvar os pontos e parar o jogo assim que alguém faça 5 pontos
 
 
 
