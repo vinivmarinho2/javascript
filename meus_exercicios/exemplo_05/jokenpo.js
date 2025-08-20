5577bc79ee9db4196084ea25ebbb69e00c7498e9
Vinícius Marinho <vini.marinho2004@gmail.com>
2025-08-20 11:31:10 -0400

    Atualizando o exercício de jokenpo

--- meus_exercicios/exemplo_05/jokenpo.js
+++ meus_exercicios/exemplo_05/jokenpo.js
@@ -1,6 +1,25 @@
 let pontos_usuario = 0
 let pontos_computador = 0
 let empate = 0
+let botoes = document.querySelectorAll("button")
+let resultado = document.querySelector("div#resultado")
+botoes.forEach((botao) => { //"ForEach" percorre todos os itens da variável "botoes"
+    // Adiciona um evento de clique para cada botão 
+    botao.addEventListener("click", (elemento) => { // O "elemento" em parênteses representa o objeto do evento que ocorreu
+        let escolha_computador = computador()
+         if (escolha_computador === "tesoura" && elemento.target.id === "papel" || escolha_computador === "papel" && elemento.target.id === "pedra" || escolha_computador === "pedra" && elemento.target.id === "tesoura") {
+            pontos_computador += 1
+            resultado.innerHTML = `Computador: ${escolha_computador}, Usuário: ${elemento.target.id}, Você perdeu!`
+            pontos_computador += 1
+         } else if (escolha_computador === elemento.target.id) {
+            resultado.innerHTML = `Empatou! Ambos escolheram ${escolha_computador}`
+         } else {
+            resultado.textContent = `Computador: ${escolha_computador}. Usuário: ${elemento.target.id}. Você venceu!`
+            pontos_usuario += 1
+         }
+    })
+})
+
 function computador() { // Sorteia a escolha do computador
     escolha = Math.floor(Math.random() * 3) // Sorteia entre "0, 1, 2"
     if (escolha === 0) {
@@ -12,33 +31,6 @@ function computador() { // Sorteia a escolha do computador
     }
     return escolha
 }
-function usuario() {
-    escolha = prompt("pedra, papel ou tesoura?").toLowerCase() // Recebe o input do usuário
-    return escolha
-}
 
 
-function rodada(escolha_usuario, escolha_computador) {
-        if (escolha_computador === "tesoura" && escolha_usuario === "papel" || escolha_computador === "papel" && escolha_usuario === "pedra" || escolha_computador === "pedra" && escolha_usuario === "tesoura") {
-            pontos_computador += 1
-            alert(` Usuário: ${escolha_usuario} \n Computador: ${escolha_computador} \n Você perdeu!`)
-        } else if (escolha_computador == escolha_usuario) {
-            empate += 1
-            alert(`Usuário: ${escolha_usuario}\nComputador: ${escolha_computador}\nEmpatou!`)
-        } else {
-            pontos_usuario += 1
-            alert(`Usuário: ${escolha_usuario} \nComputador: ${escolha_computador}\nVocê venceu`) 
-        }
-        return [pontos_computador, pontos_usuario] // retorna um array
-}
-function jogar(){
-    for(let contador = 1; contador <= 5; contador++){
-        let escolha_usuario = usuario()
-        let escolha_computador = computador()
-        rodada(escolha_usuario, escolha_computador)
-    }
-
-    alert(`Vitórias: ${pontos_usuario}\nDerrotas: ${pontos_computador}\nEmpates: ${empate}`)
-}
-jogar()
 
